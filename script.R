57255d4172dafe8943b0094c356db2e6fca60afa
Seeam2590 <seeamshahid@gmail.com>
2019-03-26 14:08:27 -0400

    Downloaded data

--- script.R
+++ script.R
@@ -1,1 +1,8 @@
 # Script for class work
+library(readr)
+library(tidyverse)
+library(dplyr)
+
+download.file(url = 'https://stacks.stanford.edu/file/druid:tr137st9964/tr137st9964_ct_hartford_2019_02_25.csv.zip', destfile = 'ct_hartford_data.rds', mode = "wb")
+
+data <- readRDS('ct_hartford_data.rds')
