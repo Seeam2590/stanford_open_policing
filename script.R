4962dc95e6484d86b529783daba9eec1004256fc
Seeam2590 <seeamshahid@gmail.com>
2019-04-05 19:26:45 -0400

    Renamed the files

--- script.R
+++ script.R
@@ -3,7 +3,10 @@ library(readr)
 library(tidyverse)
 library(dplyr)
 
-download.file(url = 'https://stacks.stanford.edu/file/druid:tr137st9964/tr137st9964_ct_hartford_2019_02_25.csv.zip', destfile = 'ct_hartford_data')
 
-data <- readRDS('ct_hartford_data.rds')
+data_main <- read_rds('tr137st9964_ct_hartford_2019_02_25.rds')
 
+
+data_main %>% group_by(subject_race, district) %>% 
+  filter(arrest_made == TRUE) %>%
+  count(arrest_made)
